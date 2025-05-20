4486870ee99f82279035ab571d5f055a491bb92e
zweird <zweir.dd@gmail.com>
2025-05-20 21:25:14 +0200

    refactor(ui): add spinner

---
+++ apps/client/components/ui/spinner/centered-spinner.tsx
@@ -0,0 +1,18 @@
+import { View } from "react-native"
+
+import { useTheme } from "@/components/contexts/theme-context"
+import { Spinner, type SpinnerProps } from "@/components/ui/spinner"
+
+type Props = SpinnerProps
+
+const CenteredSpinner = (props: Props) => {
+  const { tw } = useTheme()
+
+  return (
+    <View style={tw.style("h-full w-full items-center justify-center")}>
+      <Spinner {...props} />
+    </View>
+  )
+}
+
+export default CenteredSpinner

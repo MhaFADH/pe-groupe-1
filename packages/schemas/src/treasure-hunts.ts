37db986bd74a71bcf8bbfd8889fec9c5cdd14d47
zweird <zweir.dd@gmail.com>
2025-06-27 10:10:23 +0200

    refactor(hunts): add switch fields

--- packages/schemas/src/treasure-hunts.ts
+++ packages/schemas/src/treasure-hunts.ts
@@ -3,11 +3,8 @@ import { z } from "zod"
 export const CreateTreasureHunt = z.object({
   title: z.string().min(1, "Title is required"),
   description: z.string().optional(),
-  endDate: z.date().optional(),
+  // endDate: z.date().nullable(),
   isPublic: z.boolean(),
-  numberOfPlayers: z
-    .number()
-    .min(1)
-    .or(z.literal("").transform(() => null)),
+  numberOfPlayers: z.number().min(0),
   worldType: z.enum(["real", "cartographic"]),
 })

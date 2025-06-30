b23f209cd36b33e929c879b5811292f797bc9958
zweird <zweir.dd@gmail.com>
2025-06-30 09:42:14 +0200

    refactor(hunts): add date

--- packages/schemas/src/treasure-hunts.ts
+++ packages/schemas/src/treasure-hunts.ts
@@ -3,7 +3,7 @@ import { z } from "zod"
 export const CreateTreasureHunt = z.object({
   title: z.string().min(1, "Title is required"),
   description: z.string().optional(),
-  // endDate: z.date().nullable(),
+  endDate: z.date().nullable(),
   isPublic: z.boolean(),
   numberOfPlayers: z.number().min(0),
   worldType: z.enum(["real", "cartographic"]),

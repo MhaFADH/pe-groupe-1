3fcbcbf171b36c1050a1d9beef91a906adfb94b7
zweird <zweir.dd@gmail.com>
2025-07-11 11:03:34 +0100

    refactor(ui): set all provider in component

--- apps/client/components/providers.tsx
+++ apps/client/components/providers.tsx
@@ -1,12 +1,31 @@
 import type { ReactNode } from "react"
+import { I18nextProvider } from "react-i18next"
 import { GestureHandlerRootView } from "react-native-gesture-handler"
+
+import Auth0ProviderWrapper from "@/components/allPlatformsWrappers/auth"
+import {
+  AuthManagerProvider,
+  CustomThemeProvider,
+  LanguageProvider,
+} from "@/components/contexts"
+import i18n from "@/utils/i18n"
 
 type Props = {
   children: ReactNode
 }
 
 const Providers = ({ children }: Props) => (
-  <GestureHandlerRootView>{children}</GestureHandlerRootView>
+  <I18nextProvider i18n={i18n}>
+    <Auth0ProviderWrapper>
+      <AuthManagerProvider>
+        <LanguageProvider>
+          <CustomThemeProvider>
+            <GestureHandlerRootView>{children}</GestureHandlerRootView>{" "}
+          </CustomThemeProvider>
+        </LanguageProvider>
+      </AuthManagerProvider>
+    </Auth0ProviderWrapper>
+  </I18nextProvider>
 )
 
 export default Providers

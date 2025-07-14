1984edb18727ecdcdee474f4efddea5faf387bbb
Bruno <bruno@tulyp.io>
2025-07-14 17:47:29 +0200

    fix(knip): changed conf

--- knip.config.ts
+++ knip.config.ts
@@ -3,12 +3,15 @@ import type { KnipConfig } from "knip"
 const config: KnipConfig = {
   workspaces: {
     "apps/client": {
-      entry: ["app/**/*.tsx", "components/ui/**/index.tsx"],
+      entry: [
+        "app/**/*.tsx",
+        "components/ui/**/index.tsx",
+        "components/ar/**/*.tsx",
+      ],
     },
   },
   ignore: ["packages/db/src/drizzle/**", "tailwind.config.js"],
   ignoreDependencies: ["@babel/runtime", "@testing-library/*"],
-  ignoreBinaries: ["web", "ios", "android", "dev"],
 }
 
 export default config

42eb31ef50594eb9f850f5609fdf775031ef0720
Bruno <bruno@tulyp.io>
2025-07-14 16:38:18 +0200

    feat(ar): added artefacts

--- apps/client/metro.config.js
+++ apps/client/metro.config.js
@@ -1,5 +1,20 @@
 const { getDefaultConfig } = require("expo/metro-config")
 const { withNativeWind } = require("nativewind/metro")
+
 const config = getDefaultConfig(__dirname)
+
+config.resolver.assetExts.push(
+  "obj",
+  "mtl",
+  "mp3",
+  "JPG",
+  "vrx",
+  "hdr",
+  "gltf",
+  "glb",
+  "bin",
+  "arobject",
+  "gif",
+)
 
 module.exports = withNativeWind(config, { input: "./global.css" })
